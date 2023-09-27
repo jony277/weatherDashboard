5ba3fb925f33aaaf8b58099cc76aa51200712ab2
[Jonthan Gutierrez] <jonigutierrez741@gmail.com>
2023-09-26 20:53:15 -0400

    added refactored js againg becasue bug and also new styles and index.html code also refactored

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,101 +1,70 @@
 var key = '64f2ee2a8261daa4d9f780f5b365f275';
-var city = "Denver"
+var city = "Denver";
 
-//Grabs the current time and date
 var date = moment().format('dddd, MMMM Do YYYY');
-var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
 
 var cityHist = [];
-//Will save the text value of the search and save it to an array and storage
-$('.search').on("click", function (event) {
-	event.preventDefault();
-	city = $(this).parent('.btnPar').siblings('.textVal').val().trim();
-	if (city === "") {
-		return;
-	};
-	cityHist.push(city);
 
-	localStorage.setItem('city', JSON.stringify(cityHist));
-	fiveForecastEl.empty();
-	getHistory();
-	getWeatherToday();
+$('.search').on("click", function(event) {
+    event.preventDefault();
+    city = $('.textVal').val().trim();
+    if (city === "") {
+        return;
+    }
+    cityHist.push(city);
+    localStorage.setItem('city', JSON.stringify(cityHist));
+    getHistory();
+    getWeatherToday();
 });
 
-//Will create buttons based on search history 
 var contHistEl = $('.cityHist');
+
 function getHistory() {
-	contHistEl.empty();
-
-	for (let i = 0; i < cityHist.length; i++) {
-
-		var rowEl = $('<row>');
-		var btnEl = $('<button>').text(`${cityHist[i]}`)
-
-		rowEl.addClass('row histBtnRow');
-		btnEl.addClass('btn btn-outline-secondary histBtn');
-		btnEl.attr('type', 'button');
-
-		contHistEl.prepend(rowEl);
-		rowEl.append(btnEl);
-	} if (!city) {
-		return;
-	}
-	//Allows the buttons to start a search as well
-	$('.histBtn').on("click", function (event) {
-		event.preventDefault();
-		city = $(this).text();
-		fiveForecastEl.empty();
-		getWeatherToday();
-	});
+    contHistEl.empty();
+    for (let i = 0; i < cityHist.length; i++) {
+        var rowEl = $('<row>');
+        var btnEl = $('<button>').text(`${cityHist[i]}`);
+        rowEl.addClass('row histBtnRow');
+        btnEl.addClass('btn btn-outline-secondary histBtn');
+        btnEl.attr('type', 'button');
+        contHistEl.prepend(rowEl);
+        rowEl.append(btnEl);
+    }
+    $('.histBtn').on("click", function(event) {
+        event.preventDefault();
+        city = $(this).text();
+        getWeatherToday();
+    });
 };
 
-//Grab the main 'Today' card body.
-var cardTodayBody = $('.cardBodyToday')
-//Applies the weather data to the today card and then launches the five day forecast
+var cardTodayBody = $('.cardBodyToday');
+
 function getWeatherToday() {
-	var getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
-
-	$(cardTodayBody).empty();
-
-	$.ajax({
-		url: getUrlCurrent,
-		method: 'GET',
-	}).then(function (response) {
-		$('.cardTodayCityName').text(response.name);
-		$('.cardTodayDate').text(date);
-		//Icons
-		$('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
-		// Temperature
-		var pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
-		cardTodayBody.append(pEl);
-		//Humidity
-		var pElHumid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
-		cardTodayBody.append(pElHumid);
-		//Wind Speed
-		var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
-		cardTodayBody.append(pElWind);
-		//Set the lat and long from the searched city
-		var cityLon = response.coord.lon;
-		// console.log(cityLon);
-		var cityLat = response.coord.lat;
-		// console.log(cityLat);
-
-
-	});
+    var getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
+    $(cardTodayBody).empty();
+    $.ajax({
+        url: getUrlCurrent,
+        method: 'GET',
+    }).then(function(response) {
+        $('.cardTodayCityName').text(response.name);
+        $('.cardTodayDate').text(date);
+        $('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
+        var pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
+        cardTodayBody.append(pEl);
+        var pElHumid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
+        cardTodayBody.append(pElHumid);
+        var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
+        cardTodayBody.append(pElWind);
+    });
 };
 
-
-
-//Allows for the example data to load for Denver
 function initLoad() {
-
-	var cityHistStore = JSON.parse(localStorage.getItem('city'));
-
-	if (cityHistStore !== null) {
-		cityHist = cityHistStore
-	}
-	getHistory();
-	getWeatherToday();
+    var cityHistStore = JSON.parse(localStorage.getItem('city'));
+    if (cityHistStore !== null) {
+        cityHist = cityHistStore;
+    }
+    getHistory();
+    getWeatherToday();
 };
 
 initLoad();
